76d03a8c7bea78d97a0f0bed3508d4e575b8cc47
Vladimir Ilich Ascue Lovon <vladimir.ascue@vlaslov.com>
2022-11-23 22:34:25 -0500

    01 Tipos de datos, declaración de variables

---
+++ 01-VariablesYTitposDeDatos/src/Principal.java
@@ -0,0 +1,18 @@
+public class Principal {
+    // notacion de varianles y metodos
+    // CAMEL CASE : Se utiliza una mayuscula al inicio de cada palabra
+    //      NombresCompletos 
+    //      DireccionLegal
+    
+    public static void main(String[] args) throws Exception {
+        String nombre = "Vladimir"; // Cadena de caracteres >> texto
+        int edad = 40; // Numero entero
+        double estatura = 1.73; // Real >> Decimal
+        Boolean esSoltero = false;
+
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Edad: " + edad);
+        System.out.println("estatura: " + estatura);
+        System.out.println("Es soltero: " + esSoltero);   
+    }
+}
