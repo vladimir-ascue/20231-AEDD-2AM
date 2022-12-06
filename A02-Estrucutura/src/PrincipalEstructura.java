1ec168b68c028d3f8a7b360a570ef2ec2c354920
Vladimir Ilich Ascue Lovon <vladimir.ascue@vlaslov.com>
2022-12-06 10:34:34 -0500

    Ajuste2

--- A02-Estrucutura/src/PrincipalEstructura.java
+++ A02-Estrucutura/src/PrincipalEstructura.java
@@ -1,1 +1,20 @@
 
+public class PrincipalEstructura {
+    // Variables, funciones y métodos globales
+    // Se pueden usar en t odo el programa
+    public static String nombre = "vladimir"; // public: visible desde la misma clase y cualquier otra clase
+    protected static int edad = 40; // protected: visible desde la misma clase y las clases que heredan de ella
+    private static Boolean usaLentes = true; // private: visible unicamente desde la misma clase
+
+    // main(): Método principal en caso de ser una clase ejecutable
+    public static void main(String[] args) {
+
+        // instrucciones del método principal
+        System.out.println("Hola Java ...");
+        System.out.println();
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Edad: " + edad);
+        System.out.println("Usa lentes: " + usaLentes);
+
+    } // fin del método principal (main)
+} // fin de la clase
