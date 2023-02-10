b142f3d8f178b080061247c131b39026e2706831
vladimir-ascue <71859596+vladimir-ascue@users.noreply.github.com>
2023-02-10 11:28:01 -0500

    Update PrincipalCola.java

--- A15-CCola/src/PrincipalCola.java
+++ A15-CCola/src/PrincipalCola.java
@@ -1,11 +1,5 @@
 import java.util.*;
 
-
-Xcode
-Apple ships a binary package of Git with Xcode.
-
-Binary installer
-Tim Harper provides an installer for Git. The latest version is 2.33.0, which was released over 1 year ago, on 2021-08-30.
 public class PrincipalCola {
     public static void main(String[] args) {
         // Terminar de implementar la clase CCola
@@ -15,17 +9,6 @@ public class PrincipalCola {
         // Implementar la clase CPila
         // 1- Modificar el metodo add para que añada unicamente al final
         // 2- Implementar el método atender que solo tome el elemento del final de la Pila
-Download for macOS
-There are several options for installing Git on macOS. Note that any non-source distributions are provided by third parties, and may not be up to date with the latest source release.
-
-Homebrew
-Install homebrew if you don't already have it, then:
-$ brew install git
-
-MacPorts
-Install MacPorts if you don't already have it, then:
-$ sudo port install git
-
     
         CCola colita = new CCola();
         colita.add(0,"");
@@ -36,9 +19,3 @@ $ sudo port install git
         Stack<String> pila = new Stack<String>();
     }
 }
-
-Xcode
-Apple ships a binary package of Git with Xcode.
-
-Binary installer
-Tim Harper provides an installer for Git. The latest version is 2.33.0, which was released over 1 year ago, on 2021-08-30.
